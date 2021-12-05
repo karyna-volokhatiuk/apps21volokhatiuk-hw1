358cf8eda82a6a7558ab8c340cd1d8dac0ff3e22
Karyna Volokhatiuk <karyna.volokhatiuk@ucu.edu.ua>
2021-12-06 00:32:26 +0200

    fix err

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -8,8 +8,9 @@ public class TempSummaryStatistics {
     private final double minTemp;
     private final double maxTemp;
 
-    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp){
-        this.avgTemp =avgTemp;
+    public TempSummaryStatistics(double avgTemp,
+                                 double devTemp, double minTemp, double maxTemp) {
+        this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
         this.maxTemp = maxTemp;
