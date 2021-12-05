3c298c72efd1e0367cd72d96d8a2c2c2567d40b9
Karyna Volokhatiuk <karyna.volokhatiuk@ucu.edu.ua>
2021-12-05 23:09:36 +0200

    fix warnings

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -2,18 +2,18 @@ package ua.edu.ucu.tempseries;
 
 import lombok.Getter;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-import static java.lang.Math.abs;
-
 @Getter
 public class TemperatureSeriesAnalysis {
     private double[] temperatureSeries;
-    private int size = 0;
-    private int capacity = 1;
+    private int size;
+    private int capacity;
+
+    static final int MIN_TEMP = -273;
 
     public TemperatureSeriesAnalysis() {
-        double[] temperatureSeries  = {};
+        this.temperatureSeries  = new double[]{};
+        this.size = 0;
+        this.capacity = 1;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
@@ -22,60 +22,60 @@ public class TemperatureSeriesAnalysis {
         capacity = temperatureSeries.length;
     }
 
-    public double average(){
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+    public double average() {
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
         double sum = 0;
-        for (double temp : temperatureSeries){
+        for (double temp : temperatureSeries) {
             sum += temp;
         }
         return sum / temperatureSeries.length;
     }
 
     public double deviation() {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
         double average = average();
-        double sum_of_pows = 0;
-        for (double temp : temperatureSeries){
-            sum_of_pows += pow((temp-average), 2);
+        double sumOfPows = 0;
+        for (double temp : temperatureSeries) {
+            sumOfPows += Math.pow((temp-average), 2);
         }
 
-        if (sum_of_pows == 0.0){
-            return sum_of_pows;
+        if (sumOfPows == 0.0) {
+            return sumOfPows;
         }
 
-        return sqrt(sum_of_pows/(temperatureSeries.length-1));
+        return Math.sqrt(sumOfPows/(temperatureSeries.length-1));
     }
 
     public double min() {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
 
-        double min_temp = temperatureSeries[0];
-        for (double temp : temperatureSeries){
-            if (temp < min_temp){
-                min_temp = temp;
+        double minTemp = temperatureSeries[0];
+        for (double temp : temperatureSeries) {
+            if (temp < minTemp) {
+                minTemp = temp;
             }
         }
-        return min_temp;
+        return minTemp;
     }
 
     public double max() {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
 
-        double min_temp = temperatureSeries[0];
-        for (double temp : temperatureSeries){
-            if (temp > min_temp){
-                min_temp = temp;
+        double minTemp = temperatureSeries[0];
+        for (double temp : temperatureSeries) {
+            if (temp > minTemp) {
+                minTemp = temp;
             }
         }
-        return min_temp;
+        return minTemp;
     }
 
     public double findTempClosestToZero() {
@@ -83,97 +83,97 @@ public class TemperatureSeriesAnalysis {
     }
 
     public double findTempClosestToValue(double tempValue) {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
 
-        double closest_to_value = temperatureSeries[0];
-        for (double temp : temperatureSeries){
-            if (abs(temp - tempValue) < abs(closest_to_value - tempValue)){
-                closest_to_value = temp;
-            } else if (abs(temp - tempValue) == abs(closest_to_value - tempValue)){
-                if (temp > 0){
-                    closest_to_value = temp;
+        double closestToValue = temperatureSeries[0];
+        for (double temp : temperatureSeries) {
+            if (Math.abs(temp - tempValue) < Math.abs(closestToValue - tempValue)) {
+                closestToValue = temp;
+            } else if (Math.abs(temp - tempValue) == Math.abs(closestToValue - tempValue)) {
+                if (temp > 0) {
+                    closestToValue = temp;
                 }
             }
         }
-        return closest_to_value;
+        return closestToValue;
     }
 
     public double[] findTempsLessThen(double tempValue) {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
-        int size = 0;
+        int sizeLessArr = 0;
         for (double temp : temperatureSeries) {
             if (temp < tempValue) {
-                size += 1;
+                sizeLessArr += 1;
             }
         }
 
-        double[] temps_less_then = new double[size];
+        double[] tempsLessThen = new double[sizeLessArr];
         int idx = 0;
 
         for (double temp : temperatureSeries) {
             if (temp < tempValue) {
-                temps_less_then[idx++] = temp;
+                tempsLessThen[idx++] = temp;
             }
         }
-        return temps_less_then;
+        return tempsLessThen;
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
-        int size = 0;
+        int sizeGreaterArr = 0;
         for (double temp : temperatureSeries) {
             if (temp > tempValue) {
-                size += 1;
+                sizeGreaterArr += 1;
             }
         }
 
-        double[] temps_greater_then = new double[size];
+        double[] tempsGreaterThen = new double[sizeGreaterArr];
         int idx = 0;
 
         for (double temp : temperatureSeries) {
             if (temp > tempValue) {
-                temps_greater_then[idx++] = temp;
+                tempsGreaterThen[idx++] = temp;
             }
         }
-        return temps_greater_then;
+        return tempsGreaterThen;
     }
 
     public TempSummaryStatistics summaryStatistics() {
-        if (temperatureSeries.length == 0){
-            throw new IllegalArgumentException("List of temperatures is empty.");
+        if (temperatureSeries.length == 0) {
+            throw new IllegalArgumentException("List of temperatures is empty");
         }
         return new TempSummaryStatistics(average(), deviation(), min(), max());
     }
 
     public int addTemps(double... temps) {
-        int size_of_add = 0;
+        int sizeOfAdd = 0;
 
         for (double temp : temps) {
-            if (temp < -273) {
+            if (temp < MIN_TEMP) {
                 return size;
             }
-            size_of_add++;
+            sizeOfAdd++;
         }
 
-        if (size + size_of_add > capacity){
-            while (size + size_of_add > capacity){
+        if (size + sizeOfAdd > capacity) {
+            while (size + sizeOfAdd > capacity) {
                 capacity = 2 * capacity;
             }
 
-            double[] new_temp_list = new double[capacity];
-            for (int i = 0; i < size; i++){
-                new_temp_list[i] = temperatureSeries[i];
+            double[] newTempList = new double[capacity];
+            for (int i = 0; i < size; i++) {
+                newTempList[i] = temperatureSeries[i];
             }
-            temperatureSeries = new_temp_list;
+            temperatureSeries = newTempList;
         }
 
-        for (double temp : temps){
+        for (double temp : temps) {
             temperatureSeries[size++] = temp;
         }
         return size;
